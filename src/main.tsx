8a77a3311dfac04f5a3f4eafd951df6c96d7fac6
WoodCut <woodcutapp@gmail.com>
2025-03-15 11:41:37 -0700

    Switch to hash router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import '@/i18n'
 
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -12,9 +12,15 @@ declare module '@tanstack/react-router' {
   }
 }
 
-const router = createRouter({ routeTree, defaultPreload: 'intent' })
+const router = createRouter({
+  history: createHashHistory(),
+  routeTree,
+  defaultPreload: 'intent',
+})
+
 const rootElement = document.getElementById('root')!
 const root = createRoot(rootElement)
+
 root.render((
   <StrictMode>
     <RouterProvider router={router} />
